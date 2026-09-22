7870d00c7bc8efcb6e58e5910fe5d973496dadd6
serc-chile <serc.chile@uc.cl>
2026-09-21 22:47:51 -0300

    Add files via upload

--- sercpy/demand_profile.py
+++ sercpy/demand_profile.py
@@ -1413,6 +1413,12 @@ class DemandProfile:
     @staticmethod
     def define_temp_to_factor_func(dependence_coeff: float) -> Callable[ float ]:
         """
+        Define a Temperature-to-Demand-Factor function.
+        
+        One of the main features of DemandProfile instances is that they are capable of modeling how thermal demand varies because of changes in ambient temperature.
+        
+        The method used by SERCpy is based on the `Paper presented by Jesper et al. (doi: 10.1016/j.ecmx.2021.100085) <https://www.sciencedirect.com/science/article/pii/S2590174521000106>`_ 
+        
         Function that takes a numeric value from 0 two 3 (both limits as well as non-integer values are allowed), and returns another function, which computes a scalar factor to take into account the dependence of thermal demand on ambient temperature.
         
         The value taken by this function could be interpreted as a 'dependece coefficient' of the thermal demand on the daily average ambient temperature.
